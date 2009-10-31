1cbf6e1365ab8746f6990153ca7fe338249bbfe5
sebastian <sebastian@b85fd165-c0aa-824e-afc9-67901c46276c>
2009-10-31 11:06:32 +0000

    Simple Url Collection initial commit
    git-svn-id: file:///c/Downloads/UrlCollector.svn@1 b85fd165-c0aa-824e-afc9-67901c46276c

---
+++ UrlCollector/UrlCollector/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace UrlCollector {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Active";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(933, 678);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 725);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Url Collector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ UrlCollector/UrlCollector/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UrlCollector {
+
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+            this.checkBox1_CheckedChanged(null, null);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            this.timer1.Enabled = this.checkBox1.Checked;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            if (Clipboard.ContainsText()) {
+                string text = Clipboard.GetText();
+                if ((text.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase)
+                        || text.StartsWith("https://", StringComparison.InvariantCultureIgnoreCase)
+                        || text.StartsWith("ftp://", StringComparison.InvariantCultureIgnoreCase))
+                    && (!text.Contains('\n'))) {
+                    this.richTextBox1.AppendText(text + "\n");
+                    Clipboard.Clear();
+                }
+            }
+        }
+
+    }
+
+}

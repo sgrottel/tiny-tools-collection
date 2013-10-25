563593d46d4d928ce87c2e94bf990971747cd5e8
Sebastian <Sebastian@b2e646ed-e95d-f140-bc41-ae726a461bf7>
2013-10-25 13:22:03 +0000

    Preparing to support long paths
    git-svn-id: file://zecora/Repos/_deprecated_/FileBookmark.svn@12 b2e646ed-e95d-f140-bc41-ae726a461bf7

---
+++ src/IOUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileBookmark {
+
+    /// <summary>
+    /// Utility class for managing long file names
+    /// </summary>
+    internal static class IOUtility {
+    }
+
+}

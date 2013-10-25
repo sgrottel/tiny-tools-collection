e381eea43fd995bc8f240dccdf518de1f0c92527
Sebastian <Sebastian@b2e646ed-e95d-f140-bc41-ae726a461bf7>
2013-10-25 13:49:02 +0000

    Path operations reimplemented using simple substringing
    git-svn-id: file://zecora/Repos/_deprecated_/FileBookmark.svn@15 b2e646ed-e95d-f140-bc41-ae726a461bf7

--- src/IOUtility.cs
+++ src/IOUtility.cs
@@ -10,24 +10,38 @@ namespace SG.FileBookmark {
     /// </summary>
     internal static class IOUtility {
 
+        /// <summary>
+        /// Answer the file name extension of 'path', i.e. the string portion
+        /// including and after the last period
+        /// </summary>
+        /// <param name="path">The input path</param>
+        /// <returns>The file name extension</returns>
+        internal static string GetFileExtension(string path) {
+            int lastDot = path.LastIndexOf('.');
+            return (lastDot >= 0) ? path.Substring(lastDot) : string.Empty;
+        }
+
+        /// <summary>
+        /// Answer the directory name containing the element described by
+        /// 'path', i.e. the string portion before the last directory
+        /// separator
+        /// </summary>
+        /// <param name="path">The input path</param>
+        /// <returns>The directory name containing the element of 'path'
+        /// </returns>
+        internal static string GetDirectoryName(string path) {
+            int lastSlash = path.LastIndexOf(System.IO.Path.DirectorySeparatorChar);
+            if (lastSlash < 0) {
+                lastSlash = path.LastIndexOf(System.IO.Path.AltDirectorySeparatorChar);
+                if (lastSlash < 0) {
+                    return string.Empty;
+                }
+            }
+            return path.Substring(0, lastSlash);
+        }
+
         internal static bool FileExists(string path) {
             return System.IO.File.Exists(path);
-        }
-
-        internal static string GetFileExtension(string path) {
-            return System.IO.Path.GetExtension(path);
-        }
-
-        internal static string GetDirectoryName(string path) {
-            return System.IO.Path.GetDirectoryName(path);
-        }
-
-        internal static string[] GetFiles(string directory, string pattern) {
-            return System.IO.Directory.GetFiles(directory, pattern);
-        }
-
-        internal static void CreateFile(string path) {
-            System.IO.File.Create(path).Close();
         }
 
         internal static long FileSize(string path) {
@@ -36,6 +50,14 @@ namespace SG.FileBookmark {
                 throw new Exception("Unable to access file");
             }
             return info.Length;
+        }
+
+        internal static string[] GetFiles(string directory, string pattern) {
+            return System.IO.Directory.GetFiles(directory, pattern);
+        }
+
+        internal static void CreateFile(string path) {
+            System.IO.File.Create(path).Close();
         }
 
         internal static void DeleteFile(string path) {

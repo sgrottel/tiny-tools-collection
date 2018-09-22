1dca38de4715047710018cead0697e4dd2cef34c
SGrottel <webmaster@sgrottel.de>
2018-09-22 11:30:28 +0200

    Basic UI design

--- scfeu/MainWindow.xaml.cs
+++ scfeu/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,32 @@ namespace scfeu {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : Window, INotifyPropertyChanged {
+
+		/// <summary>
+		/// Flag to control `IsEnabled` of almost all UI
+		/// </summary>
+		private bool isUiInteractive = true;
+		public bool IsUiInteractive {
+			get { return isUiInteractive; }
+			set {
+				if (isUiInteractive != value) {
+					isUiInteractive = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsUiInteractive)));
+				}
+			}
+		}
+
 		public MainWindow() {
 			InitializeComponent();
+
+			progress.Value = 10.0;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void ScanDirectoryButton_Click(object sender, RoutedEventArgs e) {
+			IsUiInteractive = !IsUiInteractive;
 		}
 	}
 }

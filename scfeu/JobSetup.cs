0f4091cde49282641a594cb4fec11e6f0d849d32
SGrottel <webmaster@sgrottel.de>
2018-10-01 18:03:07 +0200

    Added the more seldomly used shader file extensions

--- scfeu/JobSetup.cs
+++ scfeu/JobSetup.cs
@@ -14,7 +14,7 @@ namespace scfeu
 		public const string DefaultIncludePattern
 			= "*.c;*.cpp;*.cxx;*.cc;*.tli;*.tlh;*.h;*.hh;*.hpp;*.hxx;*.hh;*.inl;*.ipp;*.rc;*.resx;*.idl;*.asm;*.inc;" // C++ project and source files
 			+ "*.cs;*.resx;*.resw;*.xsd;*.wsdl;*.xaml;*.xml;*.htm;*.html;*.css;" // C# project and source files
-			+ "*.frg;*.vrt;*.geo;" // MORF shader files
+			+ "*.frg;*.vrt;*.geo;*.comp;*.tessctrl;*.tesseval;" // MORF shader files
 			+ "*.md;*.txt"; // General text files
 		public const string DefaultExcludePattern = "*.sln;.git;.vs";
 

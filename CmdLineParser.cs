b1007392b73f77b4f8222dac06a3859e1a9ead54
SGrottel <webmaster@sgrottel.de>
2021-03-13 15:44:04 +0100

    command line argument parsing

--- CmdLineParser.cs
+++ CmdLineParser.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Redate
 {
+
 	class CmdLineParser
 	{
+
+		public Program.RunMode RunMode { get; private set; } = Program.RunMode.Run;
+
+		public string RedateFile { get; private set; } = null;
+
+		public string[] Source { get; private set; } = null;
+
 		public CmdLineParser(string[] args)
 		{
+
+			if (args.Length < 1) throw new ArgumentException("You need to specify a run mode");
+
+			switch (args[0].ToLowerInvariant())
+			{
+				case "init": RunMode = Program.RunMode.Init; break;
+				case "run": RunMode = Program.RunMode.Run; break;
+				case "reg": RunMode = Program.RunMode.FileReg; return; // no more args
+				case "unreg": RunMode = Program.RunMode.FileUnreg; break; // no more args
+				default: throw new ArgumentException("Run mode must be 'init' or 'run'. Found: " + args[0]);
+			}
+
+			if (args.Length < 2) throw new ArgumentException("You need to specify a 'redate' file");
+			RedateFile = args[1];
+
+			Source = args.AsSpan(2).ToArray();
+			if (Source.Length == 0 && RunMode == Program.RunMode.Init) throw new ArgumentException("You need to specify source information for ' init'");
 		}
+
 	}
+
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,32 @@ namespace Redate
 {
 	class Program
 	{
-		static void Main(string[] args)
+		internal enum RunMode
+		{
+			Init,
+			Run,
+			FileReg,
+			FileUnreg
+		};
+
+		static int Main(string[] args)
 		{
 			Console.WriteLine("Redate");
-			CmdLineParser cmd = new CmdLineParser(args);
+			try
+			{
+				CmdLineParser cmd = new CmdLineParser(args);
+
+
+
+
+
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine("Fatal error: " + ex);
+				return -1;
+			}
+			return 0;
 		}
 	}
 }

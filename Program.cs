59bd97dac0576cdec0cb99469bcfd344f498593a
SGrottel <webmaster@sgrottel.de>
2021-03-13 14:23:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Redate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Redate");
+			CmdLineParser cmd = new CmdLineParser(args);
+		}
+	}
+}

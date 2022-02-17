4906a31bb0bb448f850324b54bdb28d5bd7718ea
Sebastian Grottel <sebastian.grottel@microsoft.com>
2022-02-17 13:56:36 +0100

    Started project on tool "LoginWhen"

---
+++ LoginWhen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LoginWhen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog systemLog = new EventLog("System");
+
+        }
+    }
+}

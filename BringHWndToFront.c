161c461d02291c12568c07efaecba7e33b334c92
SGrottel <webmaster@sgrottel.de>
2022-06-29 08:14:05 +0200

    Separated BringHWndToFront functionality in separate file (pair) for easiser reuse in other projects

---
+++ BringHWndToFront.c
@@ -0,0 +1,121 @@
+// HWndToFront
+// Freely available via Apache License, v2.0; see LICENSE file
+//
+// Copyright 2022 SGrottel (https://www.sgrottel.de)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http ://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissionsand
+// limitations under the License.
+//
+#include "BringHWndToFront.h"
+
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+struct WINDOW_TEST
+{
+	HWND hWnd;
+	RECT rect;
+	BOOL overlap;
+};
+
+static BOOL TestWindowsForOverlap(HWND oHWnd, struct WINDOW_TEST* test)
+{
+	RECT r;
+	if (GetWindowRect(oHWnd, &r))
+	{
+		if ((r.left <= test->rect.right)
+			&& (r.right >= test->rect.left)
+			&& (r.top <= test->rect.bottom)
+			&& (r.bottom >= test->rect.top))
+		{
+			test->overlap = TRUE;
+		}
+	}
+
+	return !test->overlap; // continue
+}
+
+static BOOL IsObscured(HWND hWnd)
+{
+	struct WINDOW_TEST test;
+
+	test.hWnd = hWnd;
+	test.overlap = FALSE;
+
+	if (!GetWindowRect(hWnd, &test.rect))
+	{
+		return TRUE; // unable to determine
+	}
+
+	EnumWindows((WNDENUMPROC)&TestWindowsForOverlap, (LPARAM)&test);
+
+	return test.overlap;
+}
+
+BOOL IsForegroundHWnd(HWND hWnd)
+{
+	HWND otherHWnd;
+	otherHWnd = GetForegroundWindow();
+	return (otherHWnd == hWnd);
+}
+
+BOOL BringHWndToFront(HWND hWnd, BOOL okIfUnobscured)
+{
+	// check if already in front
+	if (IsForegroundHWnd(hWnd))
+	{
+		return TRUE;
+	}
+
+	// call legacy bring to front
+	SetForegroundWindow(hWnd);
+	Sleep(100);
+	if (IsForegroundHWnd(hWnd))
+	{
+		return TRUE;
+	}
+
+	SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+	Sleep(100);
+	if (IsForegroundHWnd(hWnd))
+	{
+		return TRUE;
+	}
+
+	// check if non-obscured, which is good enough
+	if (okIfUnobscured)
+	{
+		if (!IsObscured(hWnd))
+		{
+			return TRUE;
+		}
+	}
+
+	// modern-hacky bring-to-front dance: minimize, restore
+	ShowWindow(hWnd, SW_SHOW);
+	if (IsForegroundHWnd(hWnd))
+	{
+		return 0;
+	}
+
+	BOOL maximized = IsZoomed(hWnd);
+	ShowWindow(hWnd, SW_MINIMIZE);
+	ShowWindow(hWnd, SW_SHOWMINIMIZED);
+	ShowWindow(hWnd, SW_SHOWNORMAL);
+	if (maximized)
+	{
+		ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+	}
+
+	return IsForegroundHWnd(hWnd);
+}
--- HWndToFront.c
+++ HWndToFront.c
@@ -1,6 +1,22 @@
 // HWndToFront
 // Freely available via Apache License, v2.0; see LICENSE file
 //
+// Copyright 2022 SGrottel (https://www.sgrottel.de)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http ://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissionsand
+// limitations under the License.
+//
+#include "BringHWndToFront.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,10 +24,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-
 BOOL parseArg(int argc, char const* argv[], HWND* outHWnd)
 {
-	if (argc < 2) {
+	if (argc < 2)
+	{
 		fprintf(stderr, "You need to specify the window handle to bring to front\n");
 		return FALSE;
 	}
@@ -21,20 +37,24 @@ BOOL parseArg(int argc, char const* argv[], HWND* outHWnd)
 		return FALSE;
 	}
 
-	if (argv[1][0] == 'x') {
+	if (argv[1][0] == 'x')
+	{
 		// parse string as hex
 		int hWndValue;
 		int rc = sscanf_s(argv[1], "x%x", &hWndValue);
 		*outHWnd = (HWND)((intptr_t)hWndValue);
-		if (rc != 1) {
+		if (rc != 1)
+		{
 			fprintf(stderr, "Input argument not parsable as hex number\n");
 			return FALSE;
 		}
 	}
-	else {
+	else
+	{
 		// parse string as decimal
 		*outHWnd = (HWND)((intptr_t)atoi(argv[1]));
-		if (*outHWnd == 0) {
+		if (*outHWnd == 0)
+		{
 			fprintf(stderr, "Input argument `0` (invalid) or not parsable as number\n");
 			return FALSE;
 		}
@@ -43,106 +63,33 @@ BOOL parseArg(int argc, char const* argv[], HWND* outHWnd)
 	return TRUE;
 }
 
-
-struct WINDOW_TEST {
-	HWND hWnd;
-	RECT rect;
-	BOOL overlap;
-};
-
-
-BOOL TestWindowsForOverlap(HWND oHWnd, struct WINDOW_TEST* test)
-{
-	RECT r;
-	if (GetWindowRect(oHWnd, &r)) {
-		if ((r.left <= test->rect.right)
-			&& (r.right >= test->rect.left)
-			&& (r.top <= test->rect.bottom)
-			&& (r.bottom >= test->rect.top)) {
-			test->overlap = TRUE;
-		}
-	}
-
-	return !test->overlap; // continue
-}
-
-
-BOOL IsObscured(HWND hWnd)
-{
-	struct WINDOW_TEST test;
-
-	test.hWnd = hWnd;
-	test.overlap = FALSE;
-
-	if (!GetWindowRect(hWnd, &test.rect)) {
-		return TRUE; // unable to determine
-	}
-
-	EnumWindows((WNDENUMPROC)&TestWindowsForOverlap, (LPARAM)&test);
-
-	return test.overlap;
-}
-
-
 int main(int argc, char const* argv[])
 {
 	HWND hWnd, otherHWnd;
 
 	// Cmdline parsing
-	if (!parseArg(argc, argv, &hWnd)) {
+	if (!parseArg(argc, argv, &hWnd))
+	{
 		// failed to parse; error already printed to stderr
 		return -1;
 	}
 
 	// validated `hWnd` is a valid handle to a top-level window
-	if (!IsWindow(hWnd)) {
+	if (!IsWindow(hWnd))
+	{
 		fprintf(stderr, "Specified window handle 0x%.8x is not a valid window handle.", (unsigned)((uintptr_t)hWnd));
 		return -1;
 	}
 	otherHWnd = GetParent(hWnd);
-	if (otherHWnd != NULL) {
+	if (otherHWnd != NULL)
+	{
 		fprintf(stderr, "Specified window handle 0x%.8x is not a top-level window (it has a parent window).", (unsigned)((uintptr_t)hWnd));
 		return -1;
 	}
 
-	// check if already in front
-	otherHWnd = GetForegroundWindow();
-	if (otherHWnd == hWnd) {
+	if (BringHWndToFront(hWnd, TRUE))
+	{
 		return 0;
-	}
-
-	// call legacy bring to front
-	SetForegroundWindow(hWnd);
-	Sleep(100);
-	otherHWnd = GetForegroundWindow();
-	if (otherHWnd == hWnd) {
-		return 0;
-	}
-
-	SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
-	Sleep(100);
-	otherHWnd = GetForegroundWindow();
-	if (otherHWnd == hWnd) {
-		return 0;
-	}
-
-	// check if non-obscured, which is good enough
-	if (!IsObscured(hWnd)) {
-		return 0;
-	}
-
-	// modern-hacky bring-to-front dance: minimize, restore
-	ShowWindow(hWnd, SW_SHOW);
-	if (otherHWnd == hWnd) {
-		return 0;
-	}
-
-	BOOL maximized = IsZoomed(hWnd);
-	ShowWindow(hWnd, SW_MINIMIZE);
-	ShowWindow(hWnd, SW_SHOWMINIMIZED);
-	ShowWindow(hWnd, SW_SHOWNORMAL);
-	if (maximized) {
-		ShowWindow(hWnd, SW_SHOWMAXIMIZED);
 	}
 
 	return -1;

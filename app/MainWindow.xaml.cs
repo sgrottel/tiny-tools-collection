8dc566fe7c0bf6d6732f25d84fe53ff2373662a4
SGrottel <sgrottel@sgrottel.de>
2022-09-10 11:43:46 +0200

    First implementation of the app
    Squashed commit of the following:
    
    commit 50ca58117f49b1379ae14957275b1b6ab3935219
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 11:42:59 2022 +0200
    
        Version patched to 0.1.1 to avoid build number conflicts with deprecated build run file
    
    commit af9a49448ed159f58d7f4e448ccc9e34bd537318
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 11:40:45 2022 +0200
    
        Fixed warning in ConfigFileReader
    
    commit 8ed48c7225ae00ead38154c0a860a2b7c33f0c7f
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 11:35:13 2022 +0200
    
        build-pipeline test on branch
    
    commit 801c497c4a3b0dd07f3f7fd4c5e8d6ddc6da6b4c
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 11:34:24 2022 +0200
    
        Spawning processes
    
    commit 3fad2277778ab36e0b79f81143079faaff640f2c
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 11:22:50 2022 +0200
    
        Loading config yaml
    
    commit 1a3fdeb5b3fd9c795dfb3ad1d923538541dfb506
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 10:16:27 2022 +0200
    
        Started ConfigFileReader
    
    commit 8ef81a152672f66259046a1c68d90cbfdde23074
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 10:16:09 2022 +0200
    
        yaml > yml
    
    commit 8762ee74be8d0ee6cef19c3cb3253e30df1b78aa
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 09:01:07 2022 +0200
    
        Limiting Github Actions to pull_requests and pushes to `main` and `develop`
    
    commit 43340534a6ab5f60ab8665d2c9b4ffcf78ffd390
    Author: SGrottel <sgrottel@sgrottel.de>
    Date:   Sat Sep 10 08:43:43 2022 +0200
    
        Fundamental app ui layout

--- app/MainWindow.xaml.cs
+++ app/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using LittleStarter;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration.Internal;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,12 +24,67 @@ namespace app
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
+
+		public ObservableCollection<StartupAction> Actions { get; } = new ObservableCollection<StartupAction>();
+
+		public string Messages { get; private set; } = "";
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private ConfigFileReader configFile;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			configFile = new ConfigFileReader(
+				System.IO.Path.Combine(
+					System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "",
+					"LittleStarterConfig.yaml")
+				);
+			configFile.ClearConfig += ConfigFile_ClearConfig;
+			configFile.FailedLoading += ConfigFile_FailedLoading;
+			configFile.ActionsLoaded += ConfigFile_ActionsLoaded;
+
 			InitialWindowPlacement();
+			DataContext = this;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			configFile.Start();
+		}
+
+		private void ConfigFile_ActionsLoaded(ConfigFileReader _, StartupAction[] actions)
+		{
+			AddMessage("Actions loaded.");
+			Dispatcher.Invoke(() =>
+			{
+				Actions.Clear();
+				foreach (StartupAction sa in actions) {
+					Actions.Add(sa);
+				}
+			});
+		}
+
+		private void ConfigFile_ClearConfig(ConfigFileReader _)
+		{
+			AddMessage("No configuration loaded");
+			Dispatcher.Invoke(() =>
+			{
+				Actions.Clear();
+			});
+		}
+
+		private void ConfigFile_FailedLoading(ConfigFileReader _, string errorMsg)
+		{
+			AddMessage(string.Format("Failed loading configuration: {0}", errorMsg));
+			Dispatcher.Invoke(() =>
+			{
+				Actions.Clear();
+			});
 		}
 
 		private void InitialWindowPlacement()
@@ -31,7 +92,44 @@ namespace app
 			Width = SystemParameters.WorkArea.Width / 3;
 			Left = SystemParameters.WorkArea.Left + (SystemParameters.WorkArea.Width - Width) / 2;
 			Height = SystemParameters.WorkArea.Height / 2;
-			Top = SystemParameters.WorkArea.Top + (SystemParameters.WorkArea.Height - Height) / 2;
+			Top = SystemParameters.WorkArea.Top + 2 * (SystemParameters.WorkArea.Height - Height) / 5;
+		}
+
+		internal void AddMessage(string msg)
+		{
+			Dispatcher.Invoke(() =>
+			{
+				if (!string.IsNullOrEmpty(Messages)) Messages += "\n";
+				Messages += msg;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Messages)));
+			});
+		}
+
+		private void ButtonAction_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				foreach (StartupAction a in Actions)
+				{
+					if (!a.IsSelected) continue;
+
+					ProcessStartInfo psi = new ProcessStartInfo();
+					psi.FileName = a.Filename;
+					psi.WorkingDirectory = a.WorkingDirectory;
+					foreach (string aa in a.ArgumentList) psi.ArgumentList.Add(aa);
+					psi.Verb = a.Verb;
+					psi.UseShellExecute = a.UseShellExecute;
+
+					Process.Start(psi);
+				}
+
+				Close();
+			}
+			catch(Exception ex)
+			{
+				AddMessage(string.Format("Failed: {0}", ex.ToString()));
+			}
+
 		}
 
 	}

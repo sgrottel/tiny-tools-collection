5c05091ad9a02d163ea151f25a854f5f4668f60f
SGrottel <sgrottel@sgrottel.de>
2023-05-17 08:07:18 +0200

    Github repo date is better represented by `pushedAt`

--- GithubOverview/GithubOverview.ps1
+++ GithubOverview/GithubOverview.ps1
@@ -3,7 +3,7 @@ Write-Host "🦑 Checking Github CLI authentication status"
 gh auth status
 Write-Host "If not logged in, please run: gh auth login"
 
-$repos = (gh repo list -L 10000 --json "description,id,isArchived,isFork,isPrivate,issues,latestRelease,licenseInfo,name,owner,parent,pullRequests,url,updatedAt") | ConvertFrom-Json
+$repos = (gh repo list -L 10000 --json "description,id,isArchived,isFork,isPrivate,issues,latestRelease,licenseInfo,name,owner,parent,pullRequests,url,updatedAt,pushedAt") | ConvertFrom-Json
 $repos = $repos | Sort-Object -Property @{Expression="isArchived";Descending=$false},@{Expression="isFork";Descending=$false},@{Expression="name";Descending=$false}
 
 function Write-RepoInfo($repo)
@@ -17,7 +17,7 @@ function Write-RepoInfo($repo)
 		Write-Host "]" -NoNewLine -F DarkGray -B Black
 	}
 	Write-Host " - " -NoNewLine -F DarkGray -B Black
-	$age = -(New-TimeSpan -Start (Get-Date) -End ([datetime]$repo.updatedAt)).TotalDays
+	$age = -(New-TimeSpan -Start (Get-Date) -End ([datetime]$repo.pushedAt)).TotalDays
 	$ageCol = 'DarkGray'
 	if ($age -lt 1) { $age = "Today (<1 day)"; $ageCol = 'darkgreen' }
 	elseif ($age -lt 2) { $age = "Yesterday (<2 days)"; $ageCol = 'darkgreen' }

0bb9292171fb27250806af2836a7f82f3ebdb936
SGrottel <sgrottel@sgrottel.de>
2024-06-28 18:42:22 +0200

    MessageBox for error handling

--- StarterWindow/StarterWindow.cpp
+++ StarterWindow/StarterWindow.cpp
@@ -23,10 +23,75 @@
 
 #include "Version.h"
 
-int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, PWSTR lpCmdLine, int /*nShowCmd*/)
-{
-	sgrottel::SimpleLog log;
-	sgrottel::SimpleLog::Write(log, "StarterWindow v%d.%d.%d.%d started.", STARTERWINDOW_VER_MAJOR, STARTERWINDOW_VER_MINOR, STARTERWINDOW_VER_PATCH, STARTERWINDOW_VER_BUILD);
+#include <cstdlib>
 
-	return 0;
+// https://github.com/microsoft/Windows-classic-samples/blob/main/Samples/Win7Samples/winui/shell/appshellintegration/NotificationIcon/NotificationIcon.cpp
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "comctl32.lib")
+
+namespace
+{
+
+	std::wstring mbtow(const char* s)
+	{
+		size_t slen = strlen(s);
+		std::wstring b;
+		b.resize(slen + 1, 0);
+		size_t blen{ slen };
+		errno_t suc = mbstowcs_s(&blen, b.data(), b.size(), s, slen);
+		b.resize(blen, 0);
+		return b;
+	}
+
+
+	void ErrorMessageBox(const wchar_t* msg)
+	{
+		::MessageBoxW(nullptr, msg, L"StarterWindow", MB_OK | MB_ICONERROR);
+	}
+
+}
+
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nShowCmd);
+	int retval = 0;
+
+	sgrottel::SimpleLog* logref = nullptr;
+
+	try
+	{
+		SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+		sgrottel::SimpleLog log;
+		logref = &log;
+		sgrottel::SimpleLog::Write(log, "StarterWindow v%d.%d.%d.%d started.", STARTERWINDOW_VER_MAJOR, STARTERWINDOW_VER_MINOR, STARTERWINDOW_VER_PATCH, STARTERWINDOW_VER_BUILD);
+
+
+		throw std::runtime_error("Not implemented");
+
+
+	}
+	catch (std::exception const& ex)
+	{
+		std::wstring msg{ L"Critical failure: " };
+		msg += mbtow(ex.what());
+		sgrottel::SimpleLog::Error(logref, L"EXCEPTION: %s", ex.what());
+		ErrorMessageBox(msg.c_str());
+		retval = -1;
+	}
+	catch (...)
+	{
+		sgrottel::SimpleLog::Error(logref, L"EXCEPTION: unknown");
+		ErrorMessageBox(L"Critical failure: unknown");
+		retval = -1;
+	}
+
+	return retval;
 }

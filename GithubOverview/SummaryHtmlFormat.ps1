caa33f7643e61781927cff742e9c144fee1aa2dc
SGrottel <sgrottel@sgrottel.de>
2024-10-03 11:58:54 +0200

    GithubOverview SummaryHtmlFormat.ps1 now produces a table instead of space-aligned pre-formatted text

--- GithubOverview/SummaryHtmlFormat.ps1
+++ GithubOverview/SummaryHtmlFormat.ps1
@@ -4,52 +4,24 @@ param (
     [array]$Targets 
 )
 begin {
+    $txt = "<table>`n<tr>";
     $headers = @("name","issuesCnt","hotIssuesCnt","prCnt","isFork","isArchived","isPrivate","isForkBehind","updatedAt")
-    $lengths = $headers | ForEach-Object { $_.Length }
-    $rows = ,$headers
-    $rows += ,@("====","=========","============","=====","======","==========","=========","============","=========")
-    $cols = @("lightgreen", "lightgreen")
+    foreach ($h in $headers) {
+        $txt += "<th><code>$h</code></th>";
+    }
+    $txt += "</tr>`n";
 }
 process {
-    $vals = @()
-    $col = "silver"
+    $txt += "<tr>";
     $o = $Targets[0]
     for ($i = 0; $i -lt $headers.Length; $i++) {
         $s = ($o.($headers[$i])).ToString().Trim()
-        $vals += ,$s
-        if ($lengths[$i] -lt $s.Length) { $lengths[$i] = $s.Length }
+        $txt += "<td><code>$s</code></td>";
     }
-    if (($o.issuesCnt + $o.hotIssuesCnt + $o.prCnt) -eq 0) {
-        $col = "gray"
-    }
-    if (($o.hotIssuesCnt + $o.prCnt) -gt 0) {
-        $col = "fuchsia"
-    }
-    $rows += ,$vals
-    $cols += ,$col
+    $txt += "</tr>`n";
 }
 end {
-    $txt = "<div style=`"background-color:black;padding:0.5em`"><pre><code>"
-    $r = 0;
-    for ($r = 0; $r -lt $rows.Length; $r++)
-    {
-        $c = $cols[$r]
-        $txt += "<span style=`"color:$c`">"
-        for ($i = 0; $i -lt $lengths.Length; $i++) {
-            $s = $rows[$r][$i].ToString()
-            if ($s -match "\d+") {
-                $s = $s.PadLeft($lengths[$i])
-            } else {
-                $s = $s.PadRight($lengths[$i])
-            }
-            if ($i -gt 0) {
-                $s = ' ' + $s
-            }
-            $txt += $s
-        }
-        $txt += "</span>`n"
-    }
-    $txt += "</code></pre></div>"
+    $txt += "</table>`n"
     $txt
 }
 

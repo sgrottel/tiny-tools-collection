1eefeea0a8249ac3876a93cc5697766895cf8090
SGrottel <sgrottel@sgrottel.de>
2025-03-18 11:28:29 +0100

    Added binary version information

---
+++ StartPwsh/VersionInfo.h
@@ -0,0 +1,13 @@
+//
+// VerisonInfo.h
+// version information file
+// Open Source under MIT License
+//
+#pragma once
+
+#define STARTPWSH_VER_MAJOR	0
+#define STARTPWSH_VER_MINOR	1
+#define STARTPWSH_VER_PATCH	0
+#define STARTPWSH_VER_BUILD	0
+
+#define STARTPWSH_VER_YEARSTR	"2022-2025"

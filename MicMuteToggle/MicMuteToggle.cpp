e1f4c9b44ce63b477750dc7022b5b6933b020d84
SGrottel <sgrottel@sgrottel.de>
2025-04-25 15:38:17 +0200

    WM_APPCOMMAND un-/mutes the mic as shown on the laptops keyboard-LED, which is the device in the sound-volume-settings, but not the mute in Teams.

--- MicMuteToggle/MicMuteToggle.cpp
+++ MicMuteToggle/MicMuteToggle.cpp
@@ -1,82 +1,90 @@
 // MicMuteToggle.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include <windows.h>
-#include <mmdeviceapi.h>
-#include <winrt/base.h>
-#include <Functiondiscoverykeys_devpkey.h>
+#include <Windows.h>
+//#include <mmdeviceapi.h>
+//#include <winrt/base.h>
+//#include <Functiondiscoverykeys_devpkey.h>
 #include <iostream>
 
-namespace
-{
-	struct CoInitializeScope
-	{
-		CoInitializeScope()
-		{
-			::CoInitialize(NULL);
-		}
-		~CoInitializeScope()
-		{
-			::CoUninitialize();
-		}
-	};
-}
+//namespace
+//{
+//	struct CoInitializeScope
+//	{
+//		CoInitializeScope()
+//		{
+//			::CoInitialize(NULL);
+//		}
+//		~CoInitializeScope()
+//		{
+//			::CoUninitialize();
+//		}
+//	};
+//}
 
 //
 // https://learn.microsoft.com/en-us/windows/win32/coreaudio/endpoint-volume-controls
 //
 int main()
 {
-	CoInitializeScope coInited;
-	HRESULT hr = S_OK;
+	std::cout << "Wait for it!...\n";
+	Sleep(4000);
 
-	winrt::com_ptr<IMMDeviceEnumerator> pEnumerator;
-	hr = CoCreateInstance(
-		__uuidof(MMDeviceEnumerator),
-		NULL,
-		CLSCTX_INPROC_SERVER,
-		__uuidof(IMMDeviceEnumerator),
-		pEnumerator.put_void());
-	if (FAILED(hr))
-	{
-		std::cerr << "Failed to create IMMDeviceEnumerator" << std::endl;
-		return 1;
-	}
+	HWND hWnd = GetForegroundWindow();
+	std::cout << "ForegroundWindow handle: " << reinterpret_cast<uintptr_t>(hWnd) << "\n";
 
-	winrt::com_ptr<IMMDevice> pDevice;
-	hr = pEnumerator->GetDefaultAudioEndpoint(
-		eCapture, // mic
-		eCommunications, // used for calls
-		pDevice.put());
-	if (FAILED(hr))
-	{
-		std::cerr << "Failed to GetDefaultAudioEndpoint" << std::endl;
-		return 1;
-	}
+	SendMessage(hWnd, WM_APPCOMMAND, NULL, 0x180000);
 
-	winrt::com_ptr<IPropertyStore> pProps;
-	hr = pDevice->OpenPropertyStore(STGM_READ, pProps.put());
-	if (FAILED(hr))
-	{
-		std::cerr << "Failed to OpenPropertyStore on pDevice" << std::endl;
-		return 1;
-	}
+	//CoInitializeScope coInited;
+	//HRESULT hr = S_OK;
 
-	PROPVARIANT varName;
-	PropVariantInit(&varName);
-	hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
-	if (FAILED(hr))
-	{
-		std::cerr << "Failed to PKEY_Device_FriendlyName on pDevice" << std::endl;
-		return 1;
-	}
+	//winrt::com_ptr<IMMDeviceEnumerator> pEnumerator;
+	//hr = CoCreateInstance(
+	//	__uuidof(MMDeviceEnumerator),
+	//	NULL,
+	//	CLSCTX_INPROC_SERVER,
+	//	__uuidof(IMMDeviceEnumerator),
+	//	pEnumerator.put_void());
+	//if (FAILED(hr))
+	//{
+	//	std::cerr << "Failed to create IMMDeviceEnumerator" << std::endl;
+	//	return 1;
+	//}
 
-	if (varName.vt != VT_EMPTY)
-	{
-		// Print endpoint friendly name and endpoint ID.
-		std::wcout << L"Mic.: " << varName.pwszVal << std::endl;
-	}
+	//winrt::com_ptr<IMMDevice> pDevice;
+	//hr = pEnumerator->GetDefaultAudioEndpoint(
+	//	eCapture, // mic
+	//	eCommunications, // used for calls
+	//	pDevice.put());
+	//if (FAILED(hr))
+	//{
+	//	std::cerr << "Failed to GetDefaultAudioEndpoint" << std::endl;
+	//	return 1;
+	//}
 
-	PropVariantClear(&varName);
+	//winrt::com_ptr<IPropertyStore> pProps;
+	//hr = pDevice->OpenPropertyStore(STGM_READ, pProps.put());
+	//if (FAILED(hr))
+	//{
+	//	std::cerr << "Failed to OpenPropertyStore on pDevice" << std::endl;
+	//	return 1;
+	//}
+
+	//PROPVARIANT varName;
+	//PropVariantInit(&varName);
+	//hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+	//if (FAILED(hr))
+	//{
+	//	std::cerr << "Failed to PKEY_Device_FriendlyName on pDevice" << std::endl;
+	//	return 1;
+	//}
+
+	//if (varName.vt != VT_EMPTY)
+	//{
+	//	// Print endpoint friendly name and endpoint ID.
+	//	std::wcout << L"Mic.: " << varName.pwszVal << std::endl;
+	//}
+
+	//PropVariantClear(&varName);
 
 	return 0;
 }

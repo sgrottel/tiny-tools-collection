5ab7efa06d6ee066bfc5351a6da89939bf625489
SGrottel <sgrottel@sgrottel.de>
2025-06-24 19:40:10 +0200

    Merge array of nuget-info hashtables
    closes #132

---
+++ Scripts/Merge-NugetInfo.ps1
@@ -0,0 +1,25 @@
+#
+# $csprojs = Get-Content "C:\Downloads\all-csproj.txt"  -- paths to *.csproj files
+# $csprojs | Select-Object -First 10 | Foreach-Object { C:\dev\tiny-tools\Scripts\Get-NugetInfo.ps1 $_ } | C:\dev\tiny-tools\Scripts\Merge-NugetInfo.ps1
+# $csprojs | Foreach-Object { C:\dev\tiny-tools\Scripts\Get-NugetInfo.ps1 $_ } | C:\dev\tiny-tools\Scripts\Merge-NugetInfo.ps1 | ConvertTo-Yaml
+#
+param (
+    [parameter(Position=1, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [array]$NugetInfo
+)
+begin {
+    $nugets = @{}
+}
+process {
+    foreach ($e in $NugetInfo[0].GetEnumerator())
+    {
+        $vers = ($nugets[$e.Key] = $nugets[$e.Key] ?? @{})
+        foreach ($v in $e.Value.GetEnumerator())
+        {
+            $vers[$v.Key] = ($vers[$v.Key] ?? @()) + @($v.Value)
+        }
+    }
+}
+end {
+    $nugets
+}

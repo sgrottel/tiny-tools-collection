c69612f99fbfd534746dd5f271bdfd0930fe22f8
SGrottel <sgrottel@sgrottel.de>
2025-06-24 19:45:55 +0200

    Implemented "Merge-NugetInfo.ps1 -OnlyMultiVersions"

--- Scripts/Merge-NugetInfo.ps1
+++ Scripts/Merge-NugetInfo.ps1
@@ -5,21 +5,27 @@
 #
 param (
     [parameter(Position=1, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-    [array]$NugetInfo
+    [array]$NugetInfo,
+    [switch]$OnlyMultiVersions
 )
 begin {
     $nugets = @{}
 }
 process {
-    foreach ($e in $NugetInfo[0].GetEnumerator())
-    {
+    foreach ($e in $NugetInfo[0].GetEnumerator()) {
         $vers = ($nugets[$e.Key] = $nugets[$e.Key] ?? @{})
-        foreach ($v in $e.Value.GetEnumerator())
-        {
+        foreach ($v in $e.Value.GetEnumerator()) {
             $vers[$v.Key] = ($vers[$v.Key] ?? @()) + @($v.Value)
         }
     }
 }
 end {
-    $nugets
+    if ($OnlyMultiVersions) {
+        foreach ($key in $nugets.Keys.Clone()) {
+            if ($nugets[$key].Count -le 1) {
+                $nugets.Remove($key)
+            }
+        }
+    }
+    $nugets    
 }

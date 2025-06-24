3e4cc11dd10cdcefd9305a05ea4d5aa5097ee309
SGrottel <sgrottel@sgrottel.de>
2025-06-24 20:10:10 +0200

    Also fetch latest online versions

--- Scripts/Merge-NugetInfo.ps1
+++ Scripts/Merge-NugetInfo.ps1
@@ -2,11 +2,13 @@
 # $csprojs = Get-Content "C:\Downloads\all-csproj.txt"  -- paths to *.csproj files
 # $csprojs | Select-Object -First 10 | Foreach-Object { C:\dev\tiny-tools\Scripts\Get-NugetInfo.ps1 $_ } | C:\dev\tiny-tools\Scripts\Merge-NugetInfo.ps1
 # $csprojs | Foreach-Object { C:\dev\tiny-tools\Scripts\Get-NugetInfo.ps1 $_ } | C:\dev\tiny-tools\Scripts\Merge-NugetInfo.ps1 | ConvertTo-Yaml
+# $csprojs | Foreach-Object { C:\dev\tiny-tools\Scripts\Get-NugetInfo.ps1 $_ } | C:\dev\tiny-tools\Scripts\Merge-NugetInfo.ps1 -AddNewestVersions | ConvertTo-Yaml -OutFile C:\Downloads\csproj-nuget.yaml -Force
 #
 param (
     [parameter(Position=1, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
     [array]$NugetInfo,
-    [switch]$OnlyMultiVersions
+    [switch]$OnlyMultiVersions,
+    [switch]$AddNewestVersions
 )
 begin {
     $nugets = @{}
@@ -17,9 +19,22 @@ process {
         foreach ($v in $e.Value.GetEnumerator()) {
             $vers[$v.Key] = ($vers[$v.Key] ?? @()) + @($v.Value)
         }
+        $nugets[$e.Key] = $vers
     }
 }
 end {
+    if ($AddNewestVersions) {
+        foreach ($key in $nugets.Keys.Clone()) {
+            $packageName = $key.ToLowerInvariant()
+            $url = "https://api.nuget.org/v3-flatcontainer/$packageName/index.json"
+            $response = Invoke-RestMethod -Uri $url
+            $allVersions = $response.versions
+            $latestVersion = $allVersions[-1]  # Last item is the latest
+            $nugets[$key][$latestVersion] = ($nugets[$key][$latestVersion] ?? @()) + @("*newest*preview*")
+            $latestStable = ($allVersions | Where-Object { $_ -notmatch "-" })[-1]
+            $nugets[$key][$latestStable] = ($nugets[$key][$latestStable] ?? @()) + @("*newest*stable*")
+        }
+    }
     if ($OnlyMultiVersions) {
         foreach ($key in $nugets.Keys.Clone()) {
             if ($nugets[$key].Count -le 1) {

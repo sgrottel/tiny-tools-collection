28e5e05f47d15769163b25cb7325533aba8d5fea
SGrottel <sgrottel@sgrottel.de>
2026-08-25 14:11:22 +0200

    Testing script

--- Redate/test/generate-data.ps1
+++ Redate/test/generate-data.ps1
@@ -11,12 +11,12 @@ New-Item -ItemType Directory -Path (Join-Path $dir "b") | Out-Null
 New-Item -ItemType Directory -Path (Join-Path $dir "b\c") | Out-Null
 
 Set-Content -Path (Join-Path $dir "1.txt") -Value "This is 1"
-(Get-Item (Join-Path $dir "1.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:30:00Z")
+(Get-Item (Join-Path $dir "1.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:30:00Z").ToUniversalTime()
 Set-Content -Path (Join-Path $dir "2.txt") -Value "This is 2"
-(Get-Item (Join-Path $dir "2.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:40:00Z")
+(Get-Item (Join-Path $dir "2.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:40:00Z").ToUniversalTime()
 Set-Content -Path (Join-Path $dir "a\3.txt") -Value "This is 3"
-(Get-Item (Join-Path $dir "a\3.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:50:00Z")
+(Get-Item (Join-Path $dir "a\3.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T11:50:00Z").ToUniversalTime()
 Set-Content -Path (Join-Path $dir "b\4.txt") -Value "This is 4"
-(Get-Item (Join-Path $dir "b\4.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T12:00:00Z")
+(Get-Item (Join-Path $dir "b\4.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T12:00:00Z").ToUniversalTime()
 Set-Content -Path (Join-Path $dir "b\c\5.txt") -Value "This is 5"
-(Get-Item (Join-Path $dir "b\c\5.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T12:10:00Z")
+(Get-Item (Join-Path $dir "b\c\5.txt")).LastWriteTimeUtc = [DateTime]::Parse("2026-08-25T12:10:00Z").ToUniversalTime()
--- Redate/test/run-test.ps1
+++ Redate/test/run-test.ps1
@@ -4,8 +4,22 @@ param(
     [string]$redateExe
 )
 
+$ErrorActionPreference = 'Stop'
+
 .\generate-data.ps1
 
 $dir = Join-Path $PSScriptRoot "data"
+
+if ((@(
+    ((Get-Item (Join-Path $dir "1.txt")).LastWriteTimeUtc.ToUniversalTime() - [DateTime]::Parse("2026-08-25T11:30:00Z").ToUniversalTime()),
+    ((Get-Item (Join-Path $dir "2.txt")).LastWriteTimeUtc.ToUniversalTime() - [DateTime]::Parse("2026-08-25T11:40:00Z").ToUniversalTime()),
+    ((Get-Item (Join-Path $dir "a\3.txt")).LastWriteTimeUtc.ToUniversalTime() - [DateTime]::Parse("2026-08-25T11:50:00Z").ToUniversalTime()),
+    ((Get-Item (Join-Path $dir "b\4.txt")).LastWriteTimeUtc.ToUniversalTime() - [DateTime]::Parse("2026-08-25T12:00:00Z").ToUniversalTime()),
+    ((Get-Item (Join-Path $dir "b\c\5.txt")).LastWriteTimeUtc.ToUniversalTime() - [DateTime]::Parse("2026-08-25T12:10:00Z").ToUniversalTime())
+    ) | ForEach-Object { [Math]::Abs($_.TotalSeconds) -lt 1 }) -contains $false) {
+    throw "Initial file time setup unexpected"
+}
+Write-Host "✅ Initial file time setup ok"
+
 
 throw "Not implemented"
